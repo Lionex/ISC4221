7d0f48175d17b074a51a1e95761dc568dfd56cdc
Gwen Lofman <Gwen@Lofman.co>
2017-10-25 20:23:23 -0400

    Finish problem 1

--- labs/4/lab.m
+++ labs/4/lab.m
@@ -2,9 +2,28 @@
 % Gwen Lofman
 %%
 %% Problem 1
+% edges2graph is a simple funcion which can produce a graph in adjacency
+% matrix form from a list of its edges.
+%
+% It then becomes very easy to calculate the degree and edge count,
+% assuming our graph is undirected.
 
+% Produce graph
+edge_file = fopen('walther_edges.txt');
+E = fscanf(edge_file,'%g %g', [2 inf])';
+G = edges2graph(E);
+
+% Print out degree for each node
+fprintf('\n'); fprintf('N: %2i, deg %g\n', [1:size(G,2); sum(G)]);
+
+% Print out edge count, veryfing that calculated result is equal to the
+% number of edges we read in from the file
+edgecount = numel(G(1==G))/2;
+if size(E,1) ~= edgecount, error('Unexpected edge count'); end
+fprintf('\nedge count %i\n', edgecount);
 
 %% Problem 2
+
 
 
 %% Problem 3
