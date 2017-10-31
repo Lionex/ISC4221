ad4a0ae8bece6742fb360c6cccbc0e2986000e23
Gwen Lofman <Gwen@Lofman.co>
2017-10-30 18:15:27 -0800

    Implement brute force TSP solution
    Brute-froce travelling salseman-problem solution involving
    calculating the cost of all possible patsh and finding the set
    with the lowest cost.

--- labs/4/lab.m
+++ labs/4/lab.m
@@ -54,6 +54,42 @@ else
 end
 
 %% Problem 3
+%
+% We can brute-force calculate the shortest path in the travelling
+% saleseman problem by finding the cost of every permutation in the order
+% of cities to find the set of shortest paths, or the shortest circular
+% path through the graph.
 
+clear G;
+
+E = read('tsp_edges.txt');
+E(:,3) = fscanf(fopen('tsp_edge_weights.txt'), '%g')';
+
+W = edges2weighted(E);
+
+P = perms(1:size(W,1)); % Find all possible routes
+P = [P P(:,1)]; % Add return trips to factor return cost
+
+% Convert all paths to linear indecies into the graph W
+edge2index = @(i) sub2ind(size(W),P(:,i),P(:,i+1));
+I = cell2mat(arrayfun(edge2index,1:size(W,1), 'UniformOutput',false));
+
+% Calculate the cost of every path using the linear indecies
+C = sum(W(I),2);
+M = min(C);
+
+% Determine the solution set using logical indexing against minimum cost
+S = P(C==M,1:end-1); % Ommit return trip from solutions
+
+% Print results
+fprintf('The solution set of cost %i includes the paths:\n',M);
+
+% Convert solution to an easily printable representation
+T = [reshape(char(S+64)',1,numel(S)); reshape(char(S+48)',1,numel(S))];
+% Generate a formatter on the fly with the correct number of columns
+formatter = cell2mat(arrayfun(@(x)'%c(%c) ',1:size(W,1), ...
+    'UniformOutput',false));
+% Output results with a new line at the end of each row
+fprintf([formatter '\n'],T);
 
 %% Problem 4
