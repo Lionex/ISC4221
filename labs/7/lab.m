bc56149e8b8efbdd1841461b12a01777fb975c80
Gwen Lofman <Gwen@Lofman.co>
2017-12-11 13:51:48 -0500

    Improve lab content and writeup
    Various grammer and style and formatting fixes to bring lab 7 up
    to standard.

--- labs/7/lab.m
+++ labs/7/lab.m
@@ -1,5 +1,5 @@
 %% Lab 7: Dynamic Programming
-% ISC4221, Gwen lofman
+% ISC4221, Gwen Lofman
 %%
 %% Problem 1
 %
@@ -10,32 +10,35 @@
 %      10, 20, 5, 15, 25, 3, 23, 1, 14]
 % $$
 %
-% We want to find the longest strictly decreasing subset of $A$, $L$.  To
-% do this using dynamic programming.  Dynamic programming is necessary
-% because the time complexity of the brute force approach is exponential.
+% We want to find the longest strictly decreasing $L \subset A$ using dynamic
+% programming.  Dynamic programming is necessary because the time complexity of
+% the brute force approach is exponential.
 %
 % $$
-% \mathcal{O}\left( \sum^N_{M=1} \binom{N}{M} \right) = 
-% \mathcal{O}\left( 2^N - 1 \right)
+% \mathcal{O}\left( \sum^N_{M=1} \binom{N}{M} \right) =
+% \mathcal{O}\left( 2^N - 1 \right) =
+% \mathcal{O}\left( 2^N \right)
 % $$
 %
 % This is because to find the largest possible subset for a set of numbers
 % length $N$, we must consider the permutations of length $M$ for $M = \{1,
 % 2, \ldots, N\}$. For set $A$ which has a length $25$, $2^N = 33,554,431$.
 %
-% Using dynamic programming, we first find the largest subset of $A$ at
-% each $a_i$ where $A = \{a_1, a_2, \ldots, a_i, \ldots, a_n\}$, getting
-% the maximum subset $L_{a_j}$ where $j < i$ whose last element is less
-% than $a_{i}$, and appending $a_i$ to that subset.  This recursive
-% relationship will find a maximum subset by the last element.  We then
-% only have to find the maximum length among the N resulting subsets get
-% the maximum element.
+% Using dynamic programming, we establish a recursive relationship
+% $L(j) = \max_{i<j} L(i)$ with $L(0) = 0$, from which we can find the largest
+% subset of $A$ at each $a_i$, where
+% $A = \{a_1, a_2, \ldots, a_i, \ldots, a_n\}$.  We can only choose a particular
+% $L \subset A$ if the last element of $L$ is strictly less than $a_i$, and must
+% backtrack if this condition is violated.  This recursive relationship will
+% find a maximum subset by the time we reach the last element.  We then only
+% have to find the maximum length among the $N$ resulting subsets get the
+% maximum subset or maximum subsets.
 %
 % The time complexity of this operation in worst case is
-% 
+%
 % $$
-% \mathcal{O}\left(\sum^N_{M=1} M \right) = 
-% \mathcal{O}\left(\frac{N}{2} N(N-1) \right) = 
+% \mathcal{O}\left(\sum^N_{M=1} M \right) =
+% \mathcal{O}\left(\frac{N}{2}(N-1) \right) =
 % \mathcal{O}\left(N^2\right)
 % $$
 %
@@ -49,8 +52,8 @@ if_=@(pred,tf)tf{2-pred}();
 
 % Anonymous function to print vector of integers as latex
 vec2tex = @(v) if_(numel(v) > 1, {
-    sprintf('$\\{%s%i\\}$', sprintf('%i, ',v(1:end-1)), v(end));
-    sprintf('$\\{%i\\}$',v)
+    sprintf('\\{%s%i\\}', sprintf('%i, ',v(1:end-1)), v(end));
+    sprintf('\\{%i\\}',v)
 });
 
 % Set initial values for dynamic programming problem
@@ -62,13 +65,13 @@ A = randperm(25)';
 
 fprintf('The largest strictly increasing subset of %s found is %s with %i elements.\n',vec2tex(A), vec2tex(l), n);
 
-%%%
+%%
 % For each index $i$ of our set $A = a_{i}$, the largest subset up to
 % element $a_{i}$ is listed in the table below:
 
-fprintf('\n$$\n\\begin{tabular}{ll}');
+fprintf('\n$$\n\\begin{tabular}{l|l}');
 for i=1:numel(A)
-    fprintf('\n    $L_{a_{%i}} = %i$ && %s \\\\', i, A(i), vec2tex(cell2mat(L(i,2))));
+    fprintf('\n    $a_{%2i} = %2i$ & $L_{a_{%2i}} = %s$ \\\\', i, A(i), vec2tex(cell2mat(L(i,2))));
 end
 fprintf('\n\\end{tabular}\n$$\n');
 
